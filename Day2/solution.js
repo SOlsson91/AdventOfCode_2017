c2bb5398d89534994cda5efb990427ed6632b1df
Spunkt <S.Olsson91@gmail.com>
2017-12-02 20:43:14 +0100

    First half of Day2 completed

--- Day2/solution.js
+++ Day2/solution.js
@@ -1,3 +1,9 @@
 module.exports = {
-  
+  addArrayLines: function(numbers){
+    let diff = 0;
+    for(let i = 0; i < numbers.length; ++i){
+      diff += Math.max.apply(null, numbers[i]) - Math.min.apply(null, numbers[i]);
+    }
+    return diff;
+  }
 }
