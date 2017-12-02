69e3a276b6424a4762c126b586494d902586fb95
Spunkt <S.Olsson91@gmail.com>
2017-12-02 13:57:01 +0100

    changed how to run the different days

---
+++ run.js
@@ -0,0 +1,82 @@
+const helper = require('./utils/helper');
+let day = 2;
+
+switch(day){
+  case 1:{
+    const day1 = require('./Day1/solution');
+    let numbers = helper.readFile("./Day1/numbers.txt");
+    console.log("Result if next same: ", day1.addIfNextSame(numbers));
+    console.log("Result Other Half: ", day1.addIfOtherHalfSame(numbers));
+    break;
+  }
+  case 2:{
+    const day2 = require('./Day2/solution');
+    break;
+  }
+  case 3:{
+    break;
+  }
+  case 4:{
+    break;
+  }
+  case 5:{
+    break;
+  }
+  case 6:{
+    break;
+  }
+  case 7:{
+    break;
+  }
+  case 8:{
+    break;
+  }
+  case 9:{
+    break;
+  }
+  case 10:{
+    break;
+  }
+  case 11:{
+    break;
+  }
+  case 12:{
+    break;
+  }
+  case 13:{
+    break;
+  }
+  case 14:{
+    break;
+  }
+  case 15:{
+    break;
+  }
+  case 16:{
+    break;
+  }
+  case 17:{
+    break;
+  }
+  case 18:{
+    break;
+  }
+  case 19:{
+    break;
+  }
+  case 20:{
+    break;
+  }
+  case 21:{
+    break;
+  }
+  case 22:{
+    break;
+  }
+  case 23:{
+    break;
+  }
+  case 24:{
+    break;
+  }
+}
