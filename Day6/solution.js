d1174d89b92d7e7d1b4b24baa85130affc3d5e6b
Sebastian Olsson <elbosse@Sebastians-iMac.local>
2017-12-06 13:09:15 +0100

    Cleaned upp the code

--- Day6/solution.js
+++ Day6/solution.js
@@ -4,18 +4,18 @@ module.exports = {
   memoryMove: function(inputRow, showSizeOfLoops){
     let cycles = 0;
     let scenarios = []
-    let a = inputRow[0].split("\t");
-    for(let i = 0; i < a.length; ++i){
-      a[i] = parseInt(a[i]);
-    }
-    scenarios.push(a);
     let run = true;
-    let j = 0;
-    let seenOnce = false;
+    let arr = inputRow[0].split("\t");
+
+    for(let i = 0; i < arr.length; ++i){
+      arr[i] = parseInt(arr[i]);
+    }
+    scenarios.push(arr);
     while(run){
       let row = scenarios[scenarios.length - 1].slice();
+
       let highestValue = 0;
-      for(let i = 0; i < a.length; ++i){
+      for(let i = 0; i < row.length; ++i){
         if(row[i] > row[highestValue] && highestValue !== row[i]){ highestValue = i; }
       }
 
@@ -23,14 +23,12 @@ module.exports = {
       let stopIndex = row[highestValue];
       row[highestValue] = 0;
       for(let i = 0; i < stopIndex; ++i){
-        if(index >= row.length){
-          index = 0;
-        }
+        if(index >= row.length){ index = 0; }
         row[index] += 1;
         index++;
       }
       for(let i = 0; i < scenarios.length; ++i){
-        if(helper.arraysEqual(row, scenarios[i]) && !seenOnce){
+        if(helper.arraysEqual(row, scenarios[i])){
           if(showSizeOfLoops){ console.log(scenarios.length - i) }
           run = false;
         }
