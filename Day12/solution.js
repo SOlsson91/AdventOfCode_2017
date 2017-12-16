7ef58847088eae990575fc38fe91c3776200daf9
Spunkt <S.Olsson91@gmail.com>
2017-12-16 22:43:15 +0100

    small cmmit to get a commit of the day

--- Day12/solution.js
+++ Day12/solution.js
@@ -1,5 +1,30 @@
 module.exports = {
   travellingSalesman: function(input){
-    console.log(input);
+    let values = [];
+    input.split("\n").map(x => {
+      let pipes = [];
+      let objects = x.split(" ");
+      for(let i = 2; i < objects.length; ++i){
+        pipes.push(objects[i]);
+      }
+      let v = {
+        house: objects[0],
+        pipes: pipes
+      };
+      values.push(v);
+    });
+    let groups =[];
+    for(let i = 0; i < values.length; ++i){
+      let group = [];
+      addPipesToGroup(values, i, group);
+    }
   }
+}
+
+function addPipesToGroup(values, index, group){
+  //group.push(house);
+  for(let value = 0; value < values[index].pipes.length; ++value){
+    group.push(addPipesToGroup(values, value, group));
+  }
+  return;
 }
