789141ccd2087b7b6ddbfb6a677c558710455849
Spunkt <S.Olsson91@gmail.com>
2017-12-19 21:56:31 +0100

    Try on fixing the 2

--- Day13/solution.js
+++ Day13/solution.js
@@ -1,49 +1,62 @@
 module.exports = {
-  packScanner: function(input){
+  packScanner: function(input, useDelay){
     let val = [];
     input.split("\n").map(x => {
       let objects = x.split(": ");
       let v = {
         index: Number(objects[0]),
         val: objects[1],
-        pos: 0,
+        pos: 1,
         forward: true
       };
       val.push(v);
     });
 
-    let index = 0;
-    let caught = [];
-    for(let i = 0; i <= val[val.length - 1].index; ++i){
-      //console.log("Run:", i);
-      //console.log(val[index]);
-      let active = false;
-      val.forEach(function(value, key, map){
-        if(value.forward){ value.pos++; }
-        else { value.pos--; }
+    let delay = 0;
+    let caught = Array();
+    if(useDelay){
+      let copy = val.slice();
+      while(useDelay){
+        console.log("start","(delay)", delay,"-", copy);
+        copy.forEach(function(value){
+          value.pos--;
+        });
+        caught = runPackage(copy);
+        console.log("Caught:", caught);
+        if(delay == 2){ break;}
 
-        if(i == value.index){ active = true; }
-        if(value.pos >= value.val){ value.forward = false; }
-        if(value.pos <= 1){ value.forward = true; }
+        if(caught.length == 0){ useDelay = false; delay--;}
+        delay++;
+      }
+    } else {
+      val.forEach(function(value){
+        value.pos--;
       });
-      if(val[index].pos == 1){
-        caught.push(val[index].index * val[index].val);
-      }
-
-      if(active){
-        if(val[index].pos == i){
-//            console.log("true", val[index].index, "(", val[index].pos,")");
-        }
-
-
-        index++;
-      } else {
-        //console.log(i);
-      }
-      //console.log(val);
+      caught = runPackage(val);
     }
-    return caught.reduce(function(a, b){
-      return a+b;
-    });
+    console.log("Delay:", delay);
+    return caught.reduce(function(a, b){ return a+b; });
   }
+}
+
+function runPackage(val){
+  let index = 0;
+  let caught = [];
+  for(let i = 0; i <= val[val.length - 1].index; ++i){
+    let active = false;
+    val.forEach(function(value){
+      if(value.forward){ value.pos++; }
+      else { value.pos--; }
+
+      if(i == value.index){ active = true; }
+      if(value.pos >= value.val){ value.forward = false; }
+      if(value.pos <= 1){ value.forward = true; }
+    });
+    if(val[index].pos == 1){
+      caught.push(val[index].index * val[index].val);
+    }
+    if(active){ index++; }
+  }
+  console.log(caught);
+  return caught;
 }
