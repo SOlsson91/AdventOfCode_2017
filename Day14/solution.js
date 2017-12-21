ff4d5a8e109df6fdc5f82ceb85c8b24148dd394c
Sebastian Olsson <me@sebastianolsson.com>
2017-12-21 08:57:22 +0100

    Did not understand what to do

--- Day14/solution.js
+++ Day14/solution.js
@@ -1,5 +1,16 @@
 module.exports = {
   defragmentation: function(input){
-      console.log(input);
+    input = 'a0c2017';
+    input+="-0";
+    console.log(input);
+    let code = input.split('');
+
+    let output = [];
+    for (var i = 0; i < code.length; i++) {
+      output += input[i].charCodeAt(0).toString(2) + "\n";
+    }
+    console.log(output);
+    console.log(output.length);
+
   }
 }
