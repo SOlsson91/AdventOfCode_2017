22eec94e8bda9090089fc14b2d6820fde5cb484f
Sebastian Olsson <me@sebastianolsson.com>
2017-12-21 12:17:10 +0100

    Updated the name of the folders

--- run.js
+++ run.js
@@ -3,62 +3,62 @@ let day = 14;
 
 switch(day){
   case 1:{
-    const day1 = require('./Day1/solution');
-    let numbers = helper.fileToString("./Day1/numbers.txt");
+    const day1 = require('./Day01/solution');
+    let numbers = helper.fileToString("./Day01/numbers.txt");
     console.log("Result if next same: ", day1.addIfNextSame(numbers));
     console.log("Result Other Half: ", day1.addIfOtherHalfSame(numbers));
     break;
   }
   case 2:{
-    const day2 = require('./Day2/solution');
-    let numbers = helper.fileToArray("./Day2/input.txt");
+    const day2 = require('./Day02/solution');
+    let numbers = helper.fileToArray("./Day02/input.txt");
     console.log(day2.sumOfArrayLines(numbers));
     console.log(day2.sumOfEvenArrayDiff(numbers));
     break;
   }
   case 3:{
-    const day3 = require('./Day3/solution');
+    const day3 = require('./Day03/solution');
     console.log(day3.calculateStepsToNumber(368078));
     break;
   }
   case 4:{
-    const day4 = require('./Day4/solution');
-    let rows = helper.fileToArray("./Day4/input.txt");
+    const day4 = require('./Day04/solution');
+    let rows = helper.fileToArray("./Day04/input.txt");
     console.log(day4.checkPassphrase(rows));
     console.log(day4.checkPassphraseV2(rows));
     break;
   }
   case 5:{
-    const day5 = require('./Day5/solution');
-    let rows = helper.fileToSimpleArray("./Day5/input.txt");
+    const day5 = require('./Day05/solution');
+    let rows = helper.fileToSimpleArray("./Day05/input.txt");
     console.log(day5.jumpsToEscape(rows));
-    rows = helper.fileToSimpleArray("./Day5/input.txt");
+    rows = helper.fileToSimpleArray("./Day05/input.txt");
     console.log(day5.jumpsToEscapeV2(rows));
     break;
   }
   case 6:{
-    const day6 = require('./Day6/solution');
-    let row = helper.fileToSimpleArray('./Day6/input.txt');
+    const day6 = require('./Day06/solution');
+    let row = helper.fileToSimpleArray('./Day06/input.txt');
     let showSizeOfLoops = true;
     console.log(day6.memoryMove(row, showSizeOfLoops));
     break;
   }
   case 7:{
-    const day7 = require('./Day7/solution');
-    let rows = helper.fileToSimpleString('./Day7/input.txt');
+    const day7 = require('./Day07/solution');
+    let rows = helper.fileToSimpleString('./Day07/input.txt');
     console.log(day7.recursiveTower(rows));
     console.log(day7.balanceWeight(rows));
     break;
   }
   case 8:{
-    const day8 = require('./Day8/solution');
-    let rows = helper.fileToSimpleArray('./Day8/input.txt');
+    const day8 = require('./Day08/solution');
+    let rows = helper.fileToSimpleArray('./Day08/input.txt');
     console.log(day8.registers(rows));
     break;
   }
   case 9:{
-    const day9 = require('./Day9/solution');
-    let symbols = helper.fileToSimpleString('./Day9/input.txt');
+    const day9 = require('./Day09/solution');
+    let symbols = helper.fileToSimpleString('./Day09/input.txt');
     console.log("Score: ", day9.stringProcessing(symbols));
     break;
   }
@@ -95,9 +95,18 @@ switch(day){
     break;
   }
   case 14:{
-    const day14 = require("./Day14/solution");
-    const input = "ugkiagan";
-    day14.defragmentation(input);
+    var http = require('http');
+http.get('http://elbosse.se', function (res) {
+  // If you get here, you have a response.
+  // If you want, you can check the status code here to verify that it's `200` or some other `2xx`.
+console.log(res);
+}).on('error', function(e) {
+  // Here, an error occurred.  Check `e` for the error.
+
+});;
+    //const day14 = require("./Day14/solution");
+    //const input = "ugkiagan";
+    //day14.defragmentation(input);
     break;
   }
   case 15:{
